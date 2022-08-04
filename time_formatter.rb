88408f85da5430bb19506dc104e809b8b93751e0
Skvortsovvg <vovanches@i.ua>
2022-08-04 14:40:08 +0300

    Rack app WhatTime?

---
+++ time_formatter.rb
@@ -0,0 +1,24 @@
+# # year, month, day, hour, minute, second
+# # 1970-01-01 23:59:59
+# # '%Y-%m-%d %H:%M:%S'
+class TimeFormatter
+
+  DATE = { year: "%Y", month: "%m", day: "%d" }  
+  TIME = { hour: "%H", minute: "%M", second: "%S" }  
+  
+  def self.get_date(params)
+    Time.now.strftime(DATE.select { |key, value| params.include?(key) }.values.join("-"))
+  end
+  
+  def self.get_time(params)
+    Time.now.strftime(TIME.select { |key, value| params.include?(key) }.values.join(":"))
+  end 
+
+  def self.get_date_time(params)
+    "#{self.get_date(params)} #{self.get_time(params)}"    
+  end
+
+  def self.valid_params
+   DATE.keys + TIME.keys     
+  end
+end
--- timer.rb
+++ timer.rb
@@ -1,32 +1,6 @@
 require 'logger'
 require 'uri'
-require 'rack'
-
-# # year, month, day, hour, minute, second
-# # 1970-01-01 23:59:59
-# # '%Y-%m-%d %H:%M:%S'
-
-class TimeFormatter
-
-  DATE = { year: "%Y", month: "%m", day: "%d" }  
-  TIME = { hour: "%H", minute: "%M", second: "%S" }  
-  
-  def self.get_date(params)
-    Time.now.strftime(DATE.select { |key, value| params.include?(key) }.values.join("-"))
-  end
-  
-  def self.get_time(params)
-    Time.now.strftime(TIME.select { |key, value| params.include?(key) }.values.join(":"))
-  end 
-
-  def self.get_date_time(params)
-    "#{self.get_date(params)} #{self.get_time(params)}"    
-  end
-
-  def self.valid_params
-   DATE.keys + TIME.keys     
-  end
-end
+require 'time_formatter'
 
 class App
   
